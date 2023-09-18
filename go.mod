41d89f71bbfad03c64ec2189e86b43067c028156
Fabian Beltran <joshefbeltran@gmail.com>
2023-09-18 11:39:46 -0500

    Server creado

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module rest-api-go
 go 1.21.0
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect - Modoulo para crear webTokens
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect - Modulo para crear webTokens
+	github.com/gorilla/mux v1.8.0 // indirect - Roteador y websocket
+	github.com/gorilla/websocket v1.5.0 // indirect - Websockets
+	github.com/joho/godotenv v1.5.1 // indirect - Variables de entorno
 )
--- server/server.go
+++ server/server.go
@@ -1,7 +1,63 @@
 package server
 
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Config struct {
-	Port        string
-	JWTSecret   string
-	DatabaseUrl string
+	Port        string // Puerto donde se va a ejecutar
+	JWTSecret   string // Llave secreta para generar tokens
+	DatabaseUrl string // Conexión a la Base de datos
+}
+
+// Para que sea considerado servidor debe tener un método config - Las interfaces son contratos de implementación.
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+// Constructor
+// context para entender errores en las go rutines
+// Cuando se trabajoa con GoRutins, estas son bastantes independientes, aveces no sabemos porque estan fallando
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("Port is required")
+	}
+
+	if config.JWTSecret == "" {
+		return nil, errors.New("Secret is required")
+	}
+
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("Database url is required")
+	}
+
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Println("Starting server on port", b.Config().Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAdnServer: ", err)
+	}
 }
